ad5557752d46be4aeb9effce40e834ebcc7a9a16
Vinicius Lira <viniciuslira@gmail.com>
2026-04-25 23:32:37 -0300

    atualizar o arquivo main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')    
+    return render_template('homepage.html')
 
-@app.route('/perfil')
-def perfil():
-    return 'Este é o meu perfil!'
+@app.route('/perfil/<usuario>')
+def perfil(usuario):
+    return render_template('perfil.html', usuario=usuario)
 
 if __name__ == '__main__':
     app.run(debug=True)
